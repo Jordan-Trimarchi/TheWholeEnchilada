de757ca35995adb6bdeee180d5703407f013b969
jordan-trimarchi <ajordant47@gmail.com>
2021-08-19 22:14:32 -0600

    Another one

--- backend/database/index.js
+++ backend/database/index.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 
 const connection = new Pool({
   user: 'jordant.',
-  host: '18.191.224.181',
+  host: '18.216.104.2',
   database: 'product_overview',
   password: 'jordant',
   port: 3001,
--- connect.sh
+++ connect.sh
@@ -1,8 +1,8 @@
 case $1 in
 
 postgres)
-    scp -i SDC.pem ./init.sh ec2-user@ec2-18-191-224-181.us-east-2.compute.amazonaws.com:~/init.sh
-    ssh -i SDC.pem ec2-user@ec2-18-191-224-181.us-east-2.compute.amazonaws.com
+    scp -i SDC.pem ./init.sh ec2-user@ec2-18-216-104-2.us-east-2.compute.amazonaws.com:~/init.sh
+    ssh -i SDC.pem ec2-user@ec2-18-216-104-2.us-east-2.compute.amazonaws.com
     ;;		
 node)
     scp -i SDC.pem ./init.sh ec2-user@ec2-3-135-190-98.us-east-2.compute.amazonaws.com:~/init.sh
