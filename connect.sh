c051c0383f62d74ecc7810bfdec2b63d0cee55a2
jordan-trimarchi <ajordant47@gmail.com>
2021-08-20 16:52:45 -0600

    fiexed connect.sh

--- connect.sh
+++ connect.sh
@@ -9,7 +9,8 @@ node)
     ssh -i SDC.pem ec2-user@ec2-3-135-190-98.us-east-2.compute.amazonaws.com
     ;;
 nginx)
-scp -i SDC.pem ./init.sh ec2-user@ec2-18-222-144-163.us-east-2.compute.amazonaws.com	:~/init.sh
+    scp -i SDC.pem ./init.sh ec2-user@ec2-18-222-144-163.us-east-2.compute.amazonaws.com	:~/init.sh
     ssh -i SDC.pem ec2-user@ec2-18-222-144-163.us-east-2.compute.amazonaws.com	
     ;;
+
 esac
